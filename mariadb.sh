0e1c7d55594b4241627e213ef12c08e775e4afba
andoh_web03 <shunsuke.andoh@gmail.com>
2013-11-17 14:56:41 +0000

    maintenance mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -3,21 +3,14 @@
 if rpm -qa | grep "MariaDB" 
 then
     echo "exist! MariaDB"
-    #exit
+    exit
 else
     cp cnf/mariadb.repo /etc/yum.repos.d/ 
     rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
     yum -y install MariaDB-devel MariaDB-client MariaDB-server MariaDB-common MariaDB-compat MariaDB-shared
 fi
 
-if /etc/init.d/mysql status
-then
-    echo "hell"
-    /etc/init.d/mysql stop
-    cp /etc/my.cnf.d/server.cnf /tmp/
-    cp cnf/server.cnf /etc/my.cnf.d/server.cnf 
-    /usr/bin/mysqladmin -u root password 'aug3108a'
-    /etc/init.d/mysql start
-else
-    echo "stop"
-fi
+cp /etc/my.cnf.d/server.cnf /tmp/
+cp cnf/server.cnf /etc/my.cnf.d/server.cnf 
+/usr/bin/mysqladmin -u root password 'aug3108a'
+
