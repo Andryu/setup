85997de533d564a4780496093be3c0d68e1aaa31
ando.shunsuke <shunsuke.andoh@gmail.com>
2013-12-29 00:40:43 +0000

    delete common.sh and tmux.sh

---
+++ common.sh
@@ -0,0 +1,8 @@
+# author : shunsuke andoh
+# update : 2013/11/16
+# name   : common  
+
+yum -y install wget gcc make
+yum -y install ncurses ncurses-devel
+yum -y install vim-enhanced
+cp software/*.tar.gz /usr/local/src/
